4c80112f0ef2c88d1277fca3546e49522aa78599
Marcelo Kabbalah <marceloregis.soares@gmail.com>
2020-01-03 02:54:41 -0300

    refactored folder structure

--- Day01/src/assets/js/index.js
+++ Day01/src/assets/js/index.js
@@ -1,4 +1,4 @@
-import reddit from './redditapi';
+import reddit from '../../api/redditapi';
 
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
