0bcfe1d287052c49da22b709fd61f72b63d622c2
Marcelo Kabbalah <marceloregis.soares@gmail.com>
2020-01-16 04:38:49 -0300

    feat: add day14

---
+++ day14/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Post title="Post Title" />
+    <Post title="Post Title 2" />
+  </div>
+</template>
+
+<script>
+import Post from "./components/Post";
+
+export default {
+  name: "app",
+  components: {
+    Post
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #f3f3f3;
+  #app {
+    padding: 25px;
+  }
+}
+</style>
