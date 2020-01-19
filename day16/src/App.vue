7c52de737238df93f10e0ea7099294bcc2ce4d10
Marcelo Kabbalah <marceloregis.soares@gmail.com>
2020-01-18 23:58:40 -0300

    feat: add day16

---
+++ day16/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <Header :color="color" />
+    <ColorPicker :colors="colors" @setColor="setColor" />
+    <ColorGrid :color="color" />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import ColorPicker from "@/components/ColorPicker";
+import ColorGrid from "@/components/ColorGrid";
+export default {
+  name: "app",
+  components: {
+    Header,
+    ColorPicker,
+    ColorGrid
+  },
+  data() {
+    return {
+      colors: ["#FFF", "#000", "#AF1E2D", "#00EEAE", "#FFCE00"],
+      color: "#FFFFFF"
+    };
+  },
+  methods: {
+    setColor(color) {
+      this.color = color;
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background: #f3f3f3;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
