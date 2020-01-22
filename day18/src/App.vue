8a385e6feb5c96dc824be61f48eb5d295cfee0de
Marcelo Kabbalah <marceloregis.soares@gmail.com>
2020-01-21 23:42:31 -0300

    feat: add day18

---
+++ day18/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+  <div class="container grid-lg">
+  <header class="navbar">
+  <section class="navbar-section">
+    <router-link to="/" class="btn btn-link">Home</router-link>
+    <router-link to="/kanban" class="btn btn-link">Kanban</router-link>
+  </section>
+  <section class="navbar-center">
+    <img class="img-responsive img-logo" src="@/assets/logo.png" alt="Logomarca">
+  </section>
+  <section class="navbar-section"> 
+    <a href="" class="btn btn-link">Twitter</a>
+    <a href="" class="btn btn-link">Github</a>
+  </section>  
+  </header>
+  </div>
+    <router-view class="pt-2"/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.img-logo {
+  max-width: 50px;
+}
+</style>
---
+++ day18/src/views/kanban/Kanban.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container-board">
+    <div class="columns">
+      <div class="column col-5">
+        <div class="panel">
+          <div class="panel-header">
+            <div class="panel-title">Todo</div>
+          </div>
+          <div class="panel-body">
+            <todo
+              v-for="todo in uncheckeds"
+              :todo="todo"
+              @remove="removeTodo"
+              @toggle="toggleTodo"
+              :key="todo.id"
+            />
+          </div>
+          <div class="panel-footer">
+            <button
+              @click="checkAll"
+              v-if="uncheckeds.length > 0"
+              class="btn btn-link float-right"
+            >Done All</button>
+          </div>
+        </div>
+      </div>
+      <div class="column col-5">
+        <div class="panel">
+          <div class="panel-header">
+            <div class="panel-title">Done</div>
+          </div>
+          <div class="panel-body">
+            <todo
+              v-for="todo in checkeds"
+              :todo="todo"
+              @remove="removeTodo"
+              @toggle="toggleTodo"
+              :key="todo.id"
+            />
+          </div>
+          <div class="panel-footer">
+            <button
+              @click="uncheckAll"
+              v-if="checkeds.length > 0"
+              class="btn btn-link float-right"
+            >Undone All</button>
+            <button
+              @click="removeAllCheckeds"
+              v-if="checkeds.length > 0"
+              class="btn btn-link float-right text-error"
+            >Delete All</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from "@/components/Todo";
+import { mapGetters, mapActions } from "vuex";
+export default {
+  components: { Todo },
+  computed: {
+    ...mapGetters(["uncheckeds", "checkeds"])
+  },
+  methods: {
+    ...mapActions([
+      "removeTodo",
+      "toggleTodo",
+      "checkAll",
+      "uncheckAll",
+      "removeAllCheckeds"
+    ])
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container-board {
+  height: calc(100vh - 100px);
+  padding: 10px;
+}
+.columns {
+  height: 100%;
+
+  .column .panel {
+    height: 100%;
+    border: 0;
+    box-shadow: 0 0.25rem 1rem rgba(48, 55, 66, 0.15);
+  }
+}
+</style>
